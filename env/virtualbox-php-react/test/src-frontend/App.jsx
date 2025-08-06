1b12881ae459235d8e20d3bd2263fca73e689d8e
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-08-06 17:44:03 +0000

    added react-router

--- env/virtualbox-php-react/test/src-frontend/App.jsx
+++ env/virtualbox-php-react/test/src-frontend/App.jsx
@@ -1,7 +1,20 @@
+
+import { BrowserRouter, Routes, Route } from "react-router";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello from React + Vite??!!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test1" element={<Test1 />} />
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+function Test1() {
+  return <h2>Test1</h2>;
 }
