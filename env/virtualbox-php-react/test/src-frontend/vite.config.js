e4dd32e13850d6483fdf2f4594db697f001cd21f
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-08-06 14:28:00 +0000

    dev server now working for assets

--- env/virtualbox-php-react/test/src-frontend/vite.config.js
+++ env/virtualbox-php-react/test/src-frontend/vite.config.js
@@ -2,33 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// export default defineConfig({
-//   root: '.',                  // project root for Vite
-//   base: '/assets/js/',        // where your built files will be served from
-//   plugins: [react()],
-//   build: {
-//     outDir: resolve(__dirname, '../public/assets/js'),
-//     emptyOutDir: true,
-//     manifest: true,           // generates manifest.json for server-side injection
-//     assetsDir: '',
-//     rollupOptions: {
-//       input: resolve(__dirname, 'main.jsx')
-//     }
-//   },
-//   server: {
-//     strictPort: true,
-//     port: 3000,
-//     // proxy API calls to your PHP backend:
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:8080',
-//         changeOrigin: true,
-//       }
-//     }
-//   }
-// });
-
-
 export default defineConfig(({ command }) => ({
   root: resolve(__dirname),
   base: command === 'serve' ? '/' : '/assets/js/',
@@ -39,6 +12,7 @@ export default defineConfig(({ command }) => ({
     host: '0.0.0.0',
     allowedHosts: [
       'localhost',        // if you ever use localhost:3000
+      '127.0.0.1',        // if you ever use localhost:3000
       'code',             // your VM’s hostname
       '192.168.86.180',   // or its IP
     ],
