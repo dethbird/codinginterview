74698910e840215eb0f06eef35d9d630a7beb70e
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-10-30 19:22:08 +0000

    test react small 6

--- tests/react.003/src/challenges/small/06-toggle-list/index.jsx
+++ tests/react.003/src/challenges/small/06-toggle-list/index.jsx
@@ -1,40 +1,56 @@
 
 import React, { useEffect, useRef, useState } from 'react'
 
-const defaultItems = ['Alpha','Beta','Gamma','Delta']
+const defaultItems = ['Alpha', 'Beta', 'Gamma', 'Delta']
 
 export default function ToggleList({ items = defaultItems }) {
-  const [sel, setSel] = useState([])
-  const masterRef = useRef(null)
+    const [sel, setSel] = useState([])
+    const [allChecked, setAllChecked] = useState(false)
+    const masterRef = useRef(null)
 
-  // TODO: set masterRef.current.indeterminate based on selection
-  // TODO: derive allChecked
+    // TODO: set masterRef.current.indeterminate based on selection
+    // TODO: derive allChecked
+    useEffect(() => {
+        setAllChecked(sel.length === items.length)
+    }, [sel, items]);
 
-  const toggleAll = () => {
-    // TODO
-  }
+    const toggleAll = () => {
+        setSel(s => {
+            if (s.length < items.length) {
+                return items;
+            } else {
+                return [];
+            }
+        })
+    }
 
-  const toggleOne = (label) => {
-    // TODO
-  }
+    const toggleOne = (label) => {
+        if(!sel.includes(label)) {
+            setSel(s => {
+                return [...s, label];
+            })
+        } else {
+            setSel(s => s.filter(i => i !== label))
+        }
+    }
 
-  return (
-    <div>
-      <label>
-        <input ref={masterRef} type="checkbox" /* checked={allChecked} */ onChange={toggleAll} />
-        Select All
-      </label>
-      <ul>
-        {items.map(l => (
-          <li key={l}>
+    return (
+        <div>
             <label>
-              <input type="checkbox" checked={sel.includes(l)} onChange={() => toggleOne(l)} />
-              {l}
+                <input ref={masterRef} type="checkbox" checked={allChecked} onChange={toggleAll} />
+                Select All
             </label>
-          </li>
-        ))}
-      </ul>
-      <div data-testid="summary">{sel.length} of {items.length} selected</div>
-    </div>
-  )
+            <ul>
+                {items.map(l => (
+                    <li key={l}>
+                        <label>
+                            <input type="checkbox" checked={sel.includes(l)} onChange={() => toggleOne(l)} />
+                            {l}
+                        </label>
+                    </li>
+                ))}
+            </ul>
+            <div data-testid="summary">{sel.length} of {items.length} selected</div>
+        </div>
+    )
 }
--- tests/react.003/src/challenges/small/06-toggle-list/index.test.jsx
+++ tests/react.003/src/challenges/small/06-toggle-list/index.test.jsx
@@ -3,12 +3,23 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ToggleList from './index.jsx'
 
-it('select all, reflect indeterminate, and update summary', async () => {
-  const user = userEvent.setup()
-  render(<ToggleList items={['A','B','C']} />)
-  const master = screen.getByLabelText(/Select All/)
-  await user.click(master) // select all
-  expect(screen.getByTestId('summary')).toHaveTextContent('3 of 3 selected')
-  await user.click(screen.getByLabelText('B')) // deselect one -> indeterminate
-  expect(master).not.toBeChecked()
+it('selectAll checks all items, unchecks itelsf when an item is unchecked, checks none when all selected', async () => {
+    const user = userEvent.setup()
+    render(<ToggleList items={['A', 'B', 'C']} />)
+
+    const master = screen.getByLabelText(/Select All/)
+    
+    await user.click(master) // select all
+    expect(screen.getByTestId('summary')).toHaveTextContent('3 of 3 selected')
+    
+    await user.click(screen.getByLabelText('B')) // deselect one -> indeterminate
+    expect(master).not.toBeChecked()
+
+    // check all again 
+    await user.click(master) // select all
+    expect(screen.getByTestId('summary')).toHaveTextContent('3 of 3 selected')
+
+    // select none
+    await user.click(master)
+    expect(screen.getByTestId('summary')).toHaveTextContent('0 of 3 selected')
 })
