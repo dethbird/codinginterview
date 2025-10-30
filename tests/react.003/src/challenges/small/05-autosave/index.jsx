07bb11fc00bebb4118b02971827ced84c5e909e4
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-10-30 14:12:14 +0000

    working on react small 5 question

--- tests/react.003/src/challenges/small/05-autosave/index.jsx
+++ tests/react.003/src/challenges/small/05-autosave/index.jsx
@@ -2,12 +2,17 @@
 import React, { useEffect, useState } from 'react'
 
 export default function AutosaveInput({ storageKey = 'autosave' }) {
-  const [text, setText] = useState('')
-  // TODO: load from localStorage once
-  // TODO: debounce save with setTimeout + cleanup
-  return (
-    <div>
-      <input aria-label="text" value={text} onChange={e => setText(e.target.value)} />
-    </div>
-  )
+    // TODO: load from localStorage once
+    const [text, setText] = useState(localStorage.getItem(storageKey) || '')
+    useEffect(() => {
+        console.log(text);
+        localStorage.setItem(storageKey, text)
+    }, [text]);
+    
+    // TODO: debounce save with setTimeout + cleanup
+    return (
+        <div>
+            <input aria-label="text" value={text} onChange={e => setText(e.target.value)} />
+        </div>
+    )
 }
--- tests/react.003/src/challenges/small/05-autosave/index.test.jsx
+++ tests/react.003/src/challenges/small/05-autosave/index.test.jsx
@@ -3,13 +3,18 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import AutosaveInput from './index.jsx'
 
+beforeEach(() => {
+    localStorage.clear();
+});
+
 it('saves after a pause (blur as a simple trigger for test)', async () => {
-  const user = userEvent.setup()
-  localStorage.clear()
-  render(<AutosaveInput storageKey="k" />)
-  const input = screen.getByLabelText('text')
-  await user.type(input, 'hi')
-  // For determinism: force a save without timers.
-  input.blur()
-  expect(localStorage.getItem('k')).toBe(JSON.stringify('hi'))
+    const user = userEvent.setup()
+    render(<AutosaveInput storageKey="k" />)
+
+    const input = screen.getByLabelText('text')
+    await user.type(input, 'hi')
+
+    // For determinism: force a save without timers.
+    input.blur()
+    expect(localStorage.getItem('k')).toBe(JSON.stringify('hi'))
 })
