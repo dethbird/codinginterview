ba70f81742dd3a2417bf214219c6dde54c76264f
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-10-30 13:33:03 +0000

    working on react small q4

--- tests/react.003/src/challenges/small/04-gallery/index.jsx
+++ tests/react.003/src/challenges/small/04-gallery/index.jsx
@@ -1,22 +1,54 @@
 
 import React, { useState } from 'react'
 
-export default function Gallery({ images = [] }) {
-  const [idx, setIdx] = useState(0)
-  if (!images.length) return <p>No images</p>
-  const active = images[idx]
-  return (
-    <div>
-      <img src={active.src} alt={active.alt} data-testid="main" />
-      <ul>
-        {images.map((im, i) => (
-          <li key={im.src}>
-            <button aria-current={i===idx}>
-              <img src={im.src} alt={im.alt} width="32" height="32" />
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+
+const imgs = [
+  { src: 'https://cdn.midjourney.com/292223cb-60d8-4bc2-8323-2c6f91f661c7/0_0.png', alt: 'one' },
+  { src: 'https://cdn.midjourney.com/553e5694-d574-47be-991a-1000a549b5f6/0_0.png', alt: 'two' },
+  { src: 'https://cdn.midjourney.com/96744753-2839-4683-a09f-95e76dfb0d9d/0_0.png', alt: 'three' },
+]
+
+
+export default function Gallery({ images = imgs }) {
+    const [idx, setIdx] = useState(0)
+    const handleThumnailClick = (i) => {
+        setIdx(i);
+    }
+    const handleKeyDown = (e) => {
+        if(!e.key) return;
+        if (e.key === '{ArrowLeft}') {
+            e.preventDefault();
+            setIdx(i => {
+                if (i - 1 < 0) {
+                    return images.length - 1
+                }
+                return i - 1
+            });
+        }
+        if (e.key === '{ArrowRight}') {
+            e.preventDefault();
+            setIdx(i => {
+                if (i + 1 === images.length) {
+                    return 0
+                }
+                return i + 1
+            });
+        }
+    }
+    if (!images.length) return <p>No images</p>
+    const active = images[idx]
+    return (
+        <div onKeyDown={ handleKeyDown }>
+            <img src={active.src} alt={active.alt} data-testid="main" />
+            <ul>
+                {images.map((im, i) => (
+                    <li key={im.src}>
+                        <button aria-current={i === idx} onClick={ () => handleThumnailClick(i) }>
+                            <img src={im.src} alt={im.alt} width="32" height="32" />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
--- tests/react.003/src/challenges/small/04-gallery/index.test.jsx
+++ tests/react.003/src/challenges/small/04-gallery/index.test.jsx
@@ -4,15 +4,37 @@ import userEvent from '@testing-library/user-event'
 import Gallery from './index.jsx'
 
 const imgs = [
-  { src: '1.jpg', alt: 'one' },
-  { src: '2.jpg', alt: 'two' },
-  { src: '3.jpg', alt: 'three' },
+    { src: '1.jpg', alt: 'one' },
+    { src: '2.jpg', alt: 'two' },
+    { src: '3.jpg', alt: 'three' },
 ]
 
 it('clicking a thumbnail sets the main image', async () => {
-  const user = userEvent.setup()
-  render(<Gallery images={imgs} />)
-  const thumbs = await screen.findAllByRole('button')
-  await user.click(thumbs[1])
-  expect(screen.getByTestId('main')).toHaveAttribute('src', '2.jpg')
+    const user = userEvent.setup()
+    render(<Gallery images={imgs} />)
+
+    const thumbs = await screen.findAllByRole('button')
+    await user.click(thumbs[1])
+    expect(screen.getByTestId('main')).toHaveAttribute('src', '2.jpg')
+})
+
+it('sets aria-current to true for active thumbnail', async () => {
+    const user = userEvent.setup()
+    render(<Gallery images={imgs} />)
+
+    const thumbs = await screen.findAllByRole('button')
+    await user.click(thumbs[2])
+    expect(thumbs[2]).toHaveAttribute('aria-current', "true")
+})
+
+it('Keyboard left/right changes active image', async () => {
+    const user = userEvent.setup()
+    render(<Gallery images={imgs} />)
+    const thumbs = await screen.findAllByRole('button')
+
+    // ensure focus
+    await user.click(thumbs[0])
+
+    await user.keyboard('{ArrowRight}');
+    expect(thumbs[1]).toHaveAttribute('aria-current', "true")
 })
