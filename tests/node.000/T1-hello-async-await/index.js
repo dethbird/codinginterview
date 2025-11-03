d3a3aa200a56f6d57a40916267fcd6e715b9e61d
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-11-03 17:30:48 +0000

    node.000 q 1

--- tests/node.000/T1-hello-async-await/index.js
+++ tests/node.000/T1-hello-async-await/index.js
@@ -1,9 +1,20 @@
 'use strict';
 
 function sleep(ms) {
-  // TODO: return new Promise(resolve => setTimeout(resolve, Math.max(0, ms)));
+  return new Promise((resolve, reject) => {
+    if (ms < 0) {
+      reject(new Error('Negative timeout not allowed'));
+    } else {
+      setTimeout(resolve, ms);
+    }
+  });
 }
 
 (async () => {
   // TODO: for (let i=1;i<=5;i++){ await sleep(200); console.log('tick', i); }
+  for (let i = 1; i <= 5; i++) {
+    await sleep(200);
+    // await sleep(-60); // negative not allowed
+    console.log('tick', i);
+  }
 })();
