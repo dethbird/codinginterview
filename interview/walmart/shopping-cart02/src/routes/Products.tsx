5ead21806d30c22ff3da5cec8577d302ca18eb6b
Rishi Satsangi <rishi.satsangi@gmail.com>
2026-05-20 10:27:20 -0400

    create types and add product list

--- interview/walmart/shopping-cart02/src/routes/Products.tsx
+++ interview/walmart/shopping-cart02/src/routes/Products.tsx
@@ -1,3 +1,24 @@
+import { Product } from "../types";
+
+const products: Product[] = [
+    {
+        id: '1',
+        name: 'Pizza',
+        price: 1.5,
+        description: "a slippery slice",
+        imageUrl: "https://t3.ftcdn.net/jpg/06/27/81/36/360_F_627813626_bjLLoQXww5AlKs1giaxRW8H7g2woTqll.jpg"
+
+    },
+    {
+        id: '2',
+        name: 'Taco',
+        price: 1.75,
+        description: "tortilla mania",
+        imageUrl: "https://tb-static.uber.com/prod/image-proc/processed_images/e787650692e172db315ce002132f59dd/5954bcb006b10dbfd0bc160f6370faf3.jpeg"
+
+    }
+];
+
 export default function Products() {
     return <div>Products</div>
 }
---
+++ interview/walmart/shopping-cart02/src/types.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: string
+    name: string
+    price: number
+    description?: string
+    imageUrl: string
+}
+
+export type CartItem {
+    product: Product
+    quantity: number
+}
