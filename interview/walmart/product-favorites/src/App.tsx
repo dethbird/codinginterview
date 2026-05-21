fba16016a3ceee016c5e1093c7b7f7d0d357cd84
Rishi Satsangi <rishi.satsangi@gmail.com>
2026-05-21 13:11:31 -0400

    create vite react ts product favorites app

---
+++ interview/walmart/product-favorites/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="header">
+        <h1>Favorite Products Manager</h1>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="product_list">
+        products
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
