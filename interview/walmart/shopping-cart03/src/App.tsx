e2911c503333a40e160c7edee696c3bd9db4929f
Rishi Satsangi <rishi.satsangi@gmail.com>
2026-05-21 08:49:19 -0400

    shopping cart 03

---
+++ interview/walmart/shopping-cart03/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Store</h1>
+
+      <div className="ticks"></div>
+
+      <section>
+        nav
+      </section>
+
+      <section id="routes">
+        
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
